8edec36e171d24578aa6f0fd4c9085a0a0008130
iflocky <Petrenko1702@gmail.com>
2024-06-20 15:15:42 +0300

    fix: fix header styles

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,38 +1,43 @@
 import React from 'react';
 import './header-style.css';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
-    return (
-        <header className="header">
-            <div className="top-bar">
-                <div className="location">OUR LOCATION: 3402 San Jacinto St, Houston, Texas 77004</div>
-                <div className="contact">
-                    CALL US: <a href="tel:+17135232363">(713) 523-2363</a>
-                    <a href="https://www.facebook.com" className="facebook-icon">f</a>
-                </div>
-            </div>
-            <nav className="nav-bar">
-                <div className="logo">
-                    <span className="logo-text">Autohouse of Switzerland, Inc.</span>
-                    <span className="logo-subtext">Specializing in European Autos Since 1980</span>
-                </div>
-                <ul className="nav-links">
-                    <li><a href="#home">HOME</a></li>
-                    <li><a href="#about">ABOUT</a></li>
-                    <li className="dropdown">
-                        <a href="#services">SERVICES</a>
-                        <div className="dropdown-content">
-                            <a href="#service1">Service 1</a>
-                            <a href="#service2">Service 2</a>
-                        </div>
-                    </li>
-                    <li><a href="#reviews">REVIEWS</a></li>
-                    <li><a href="#contact">CONTACT</a></li>
-                </ul>
-                <a href="#book" className="book-appointment">BOOK APPOINTMENT</a>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+
+      <div className="top-bar">
+        <div className="container">
+          <div className="location"><span>Локация:  </span>Минск, ул. Змитрока Бядули, 13, корп. 7</div>
+          <div className="contact">
+            Позвони нам: <a href="tel:+17135232363">(713) 523-2363</a>
+          </div>
+        </div>
+      </div>
+      <nav className="nav-bar">
+        <div className="container">
+          <div className="logo">
+            <Link to="/">
+              <span className="logo-subtext"></span>
+              <span className="logo-text">Fast Motors</span>
+            </Link>
+          </div>
+          <ul className="nav-links">
+            <li><Link to="/about">О нас</Link></li>
+            <li className="dropdown">
+              <div><Link to="/services">Наши сервисы</Link></div>
+              <div className="dropdown-content">
+                <Link to="/service1">Бядули 13</Link>
+              </div>
+            </li>
+            <li><Link to="/work-performed">Услуги</Link></li>
+            <li><Link to="/contacts">Контакты</Link></li>
+          </ul>
+          <Link to="#write-book" className="book-appointment">Записать на прием</Link>
+        </div>
+      </nav>
+    </header >
+  );
 };
 
 export default Header;
